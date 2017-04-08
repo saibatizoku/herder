8eba7178cdb77c4a06153faef190b31d1b18fbc1
Joaquin R <globojorro@gmail.com>
2017-04-08 08:49:50 -0500

    some tests

---
+++ tests/test_herder.rs
@@ -0,0 +1,16 @@
+extern crate herder;
+
+#[test]
+fn create_client() {
+    unimplemented!()
+}
+
+#[test]
+fn client_registration() {
+    unimplemented!()
+}
+
+#[test]
+fn client_token_authentication() {
+    unimplemented!()
+}
