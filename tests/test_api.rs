32b6be1be83e4e400cd1b7539d83265e66ed3ce2
Joaquín R <globojorro@gmail.com>
2017-04-17 12:13:27 -0500

    tests for all entitities deserialization

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -18,9 +18,75 @@ use herder::api::v1::{
     Tag
 };
 
+fn mock_account() -> Account {
+    Account {
+        account_id: 1234,
+        username: String::from("MYUSERNAME"),
+        acct: String::from("MYUSERNAME@MYREMOTEDOMAIN"),
+        display_name: String::from("jane_sanchez"),
+        note: String::from("A self-described person"),
+        url: String::from("MYRUL"),
+        avatar: String::from("MYURL.png"),
+        header: String::from("MYHEADER.png"),
+        locked: false,
+        created_at: String::from("2000-01-01T00:00:00Z"),
+        followers_count: 1234,
+        following_count: 1234,
+        statuses_count: 1234
+    }
+}
+
+fn mock_application() -> Application {
+    Application {
+        name: String::from("MYNAME"),
+        website: Some(String::from("MYURI"))
+    }
+}
+
+fn mock_status() -> Status {
+    Status {
+        status_id: 1234,
+        uri: String::from("MYURI"),
+        url: String::from("MYURL"),
+        account: mock_account(),
+        in_reply_to_id: Some(1234),
+        in_reply_to_account_id: Some(1234),
+        reblog: None,
+        content: String::from("My tooted toot!"),
+        created_at: String::from("2000-01-01T00:00:00Z"),
+        reblogs_count: 555,
+        favourites_count: 777,
+        reblogged: true,
+        favourited: true,
+        sensitive: false,
+        spoiler_text: String::from(""),
+        visibility: String::from(""),
+        media_attachments: Vec::new(),
+        mentions: Vec::new(),
+        tags: Vec::new(),
+        application: mock_application()
+    }
+}
+
 #[test]
 fn account_deserialize_from_json() {
-    unimplemented!();
+    let ojson = r#"{
+        "id": 1234,
+        "username": "MYUSERNAME",
+        "acct": "MYUSERNAME@MYREMOTEDOMAIN",
+        "display_name": "jane_sanchez",
+        "note": "A self-described person",
+        "url": "MYRUL",
+        "avatar": "MYURL.png",
+        "header": "MYHEADER.png",
+        "locked": false,
+        "created_at": "2000-01-01T00:00:00Z",
+        "followers_count": 1234,
+        "following_count": 1234,
+        "statuses_count": 1234
+    }"#;
+    let account: Account = serde_json::from_str(ojson).unwrap();
+    assert_eq!(account, mock_account());
 }
 
 #[test]
@@ -30,10 +96,7 @@ fn application_deserialize_from_json() {
                     "website": "MYURI"
                  }"#;
     let application: Application = serde_json::from_str(ojson).unwrap();
-    assert_eq!(application, Application {
-                        name: String::from("MYNAME"),
-                        website: Some(String::from("MYURI"))
-    });
+    assert_eq!(application, mock_application());
 }
 
 #[test]
@@ -48,12 +111,12 @@ fn attachment_deserialize_from_json() {
                  }"#;
     let attachment: Attachment = serde_json::from_str(ojson).unwrap();
     assert_eq!(attachment, Attachment {
-                        attachment_id: 1234,
-                        attachment_type: String::from("video"),
-                        url: String::from("MYURL"),
-                        remote_url: String::from("MYURL"),
-                        preview_url: String::from("MYURL"),
-                        text_url: String::from("MYURL")
+        attachment_id: 1234,
+        attachment_type: String::from("video"),
+        url: String::from("MYURL"),
+        remote_url: String::from("MYURL"),
+        preview_url: String::from("MYURL"),
+        text_url: String::from("MYURL")
     });
 }
 
@@ -67,16 +130,24 @@ fn card_deserialize_from_json() {
                  }"#;
     let card: Card = serde_json::from_str(ojson).unwrap();
     assert_eq!(card, Card {
-                        url: String::from("MYURL"),
-                        title: String::from("My Title"),
-                        description: String::from("My description"),
-                        image: String::from("MYIMG.png")
+        url: String::from("MYURL"),
+        title: String::from("My Title"),
+        description: String::from("My description"),
+        image: String::from("MYIMG.png")
     });
 }
 
 #[test]
 fn context_deserialize_from_json() {
-    unimplemented!();
+    let ojson = r#"{
+                    "ancestors": [],
+                    "descendants": []
+                 }"#;
+    let context: Context = serde_json::from_str(ojson).unwrap();
+    assert_eq!(context, Context {
+        ancestors: Vec::new(),
+        descendants: Vec::new()
+    });
 }
 
 #[test]
@@ -86,7 +157,7 @@ fn error_deserialize_from_json() {
                  }"#;
     let error: Error = serde_json::from_str(ojson).unwrap();
     assert_eq!(error, Error {
-                        error: String::from("Error message")
+        error: String::from("Error message")
     });
 }
 
@@ -100,10 +171,10 @@ fn instance_deserialize_from_json() {
                  }"#;
     let instance: Instance = serde_json::from_str(ojson).unwrap();
     assert_eq!(instance, Instance {
-                        url: String::from("MYURL"),
-                        title: String::from("My Title"),
-                        description: String::from("My description"),
-                        email: String::from("MYEMAIL")
+        url: String::from("MYURL"),
+        title: String::from("My Title"),
+        description: String::from("My description"),
+        email: String::from("MYEMAIL")
     });
 }
 
@@ -117,16 +188,44 @@ fn mention_deserialize_from_json() {
                  }"#;
     let mention: Mention = serde_json::from_str(ojson).unwrap();
     assert_eq!(mention, Mention {
-                        mention_id: 1234,
-                        url: String::from("MYURL"),
-                        username: String::from("MYUSERNAME"),
-                        acct: String::from("MYUSERNAME@REMOTEDOMAIN")
+        mention_id: 1234,
+        url: String::from("MYURL"),
+        username: String::from("MYUSERNAME"),
+        acct: String::from("MYUSERNAME@REMOTEDOMAIN")
     });
 }
 
 #[test]
 fn notification_deserialize_from_json() {
-    unimplemented!();
+    let ojson = r#"{
+                    "id": 1234,
+                    "type": "NOTIFICATIONTYPE",
+                    "created_at": "2000-01-01T00:00:00Z",
+                    "account": {
+                        "id": 1234,
+                        "username": "MYUSERNAME",
+                        "acct": "MYUSERNAME@MYREMOTEDOMAIN",
+                        "display_name": "jane_sanchez",
+                        "note": "A self-described person",
+                        "url": "MYRUL",
+                        "avatar": "MYURL.png",
+                        "header": "MYHEADER.png",
+                        "locked": false,
+                        "created_at": "2000-01-01T00:00:00Z",
+                        "followers_count": 1234,
+                        "following_count": 1234,
+                        "statuses_count": 1234
+                    },
+                    "status": null
+                 }"#;
+    let notification: Notification = serde_json::from_str(ojson).unwrap();
+    assert_eq!(notification, Notification {
+        notification_id: 1234,
+        notification_type: String::from("NOTIFICATIONTYPE"),
+        created_at: String::from("2000-01-01T00:00:00Z"),
+        account: mock_account(),
+        status: None
+    });
 }
 
 #[test]
@@ -141,12 +240,12 @@ fn relationship_deserialize_from_json() {
                  }"#;
     let relationship: Relationship = serde_json::from_str(ojson).unwrap();
     assert_eq!(relationship, Relationship {
-                        relationship_id: 1234,
-                        following: true,
-                        followed_by: true,
-                        blocking: true,
-                        muting: true,
-                        requested: true
+        relationship_id: 1234,
+        following: true,
+        followed_by: true,
+        blocking: true,
+        muting: true,
+        requested: true
     });
 }
 
@@ -158,19 +257,69 @@ fn report_deserialize_from_json() {
                  }"#;
     let report: Report = serde_json::from_str(ojson).unwrap();
     assert_eq!(report, Report {
-                        report_id: 1234,
-                        action_taken: String::from("Some action taken")
+        report_id: 1234,
+        action_taken: String::from("Some action taken")
     });
 }
 
 #[test]
 fn results_deserialize_from_json() {
-    unimplemented!();
+    let ojson = r#"{
+                    "accounts": [],
+                    "statuses": [],
+                    "hashtags": []
+                 }"#;
+    let results: Results = serde_json::from_str(ojson).unwrap();
+    assert_eq!(results, Results {
+        accounts: Vec::new(),
+        statuses: Vec::new(),
+        hashtags: Vec::new()
+    });
 }
 
 #[test]
 fn status_deserialize_from_json() {
-    unimplemented!();
+    let ojson = r#"{
+                    "id": 1234,
+                    "uri": "MYURI",
+                    "url": "MYURL",
+                    "account": {
+                        "id": 1234,
+                        "username": "MYUSERNAME",
+                        "acct": "MYUSERNAME@MYREMOTEDOMAIN",
+                        "display_name": "jane_sanchez",
+                        "note": "A self-described person",
+                        "url": "MYRUL",
+                        "avatar": "MYURL.png",
+                        "header": "MYHEADER.png",
+                        "locked": false,
+                        "created_at": "2000-01-01T00:00:00Z",
+                        "followers_count": 1234,
+                        "following_count": 1234,
+                        "statuses_count": 1234
+                    },
+                    "in_reply_to_id": 1234,
+                    "in_reply_to_account_id": 1234,
+                    "reblog": null,
+                    "content": "My tooted toot!",
+                    "created_at": "2000-01-01T00:00:00Z",
+                    "reblogs_count": 555,
+                    "favourites_count": 777,
+                    "reblogged": true,
+                    "favourited": true,
+                    "sensitive": false,
+                    "spoiler_text": "",
+                    "visibility": "",
+                    "media_attachments": [],
+                    "mentions": [],
+                    "tags": [],
+                    "application": {
+                        "name": "MYNAME",
+                        "website": "MYURI"
+                    }
+                 }"#;
+    let status: Status = serde_json::from_str(ojson).unwrap();
+    assert_eq!(status, mock_status());
 }
 
 #[test]
@@ -181,7 +330,7 @@ fn tag_deserialize_from_json() {
                  }"#;
     let tag: Tag = serde_json::from_str(ojson).unwrap();
     assert_eq!(tag, Tag {
-                        name: String::from("rustlang"),
-                        url: String::from("MYURL")
+        name: String::from("rustlang"),
+        url: String::from("MYURL")
     });
 }
