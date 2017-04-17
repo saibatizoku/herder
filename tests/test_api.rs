208ad4e4662f1e6294ad6e8fe527cdcbff11cd4d
Joaquín R <globojorro@gmail.com>
2017-04-17 01:45:56 -0500

    tests json deserialization of Application, Attachment, Card entities

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -1,17 +1,59 @@
 extern crate herder;
 extern crate serde_json;
 
-use herder::api;
+use herder::api::v1::{
+    Application,
+    Attachment,
+    Card
+};
 
 #[test]
-fn account_from_json() {
+fn account_deserialize_from_json() {
     let ojson = r#"{
                     "name": "MYNAME",
                     "website": "MYURI"
                  }"#;
-    let application: api::v1::Application = serde_json::from_str(ojson).unwrap();
-    assert_eq!(application, api::v1::Application {
+    let application: Application = serde_json::from_str(ojson).unwrap();
+    assert_eq!(application, Application {
                         name: String::from("MYNAME"),
                         website: Some(String::from("MYURI"))
+    });
+}
+
+#[test]
+fn attachment_deserialize_from_json() {
+    let ojson = r#"{
+                    "id": 1234,
+                    "type": "video",
+                    "url": "MYURL",
+                    "remote_url": "MYURL",
+                    "preview_url": "MYURL",
+                    "text_url": "MYURL"
+                 }"#;
+    let attachment: Attachment = serde_json::from_str(ojson).unwrap();
+    assert_eq!(attachment, Attachment {
+                        attachment_id: 1234,
+                        attachment_type: String::from("video"),
+                        url: String::from("MYURL"),
+                        remote_url: String::from("MYURL"),
+                        preview_url: String::from("MYURL"),
+                        text_url: String::from("MYURL")
+    });
+}
+
+#[test]
+fn card_deserialize_from_json() {
+    let ojson = r#"{
+                    "url": "MYURL",
+                    "title": "My Title",
+                    "description": "My description",
+                    "image": "MYIMG.png"
+                 }"#;
+    let card: Card = serde_json::from_str(ojson).unwrap();
+    assert_eq!(card, Card {
+                        url: String::from("MYURL"),
+                        title: String::from("My Title"),
+                        description: String::from("My description"),
+                        image: String::from("MYIMG.png")
     });
 }
