970eb54ff71a5a53170d8a3454c4af03cb191066
Joaquín R <globojorro@gmail.com>
2017-04-17 20:49:25 -0500

    add tests for API client

--- tests/test_client.rs
+++ tests/test_client.rs
@@ -1,9 +1,24 @@
 extern crate herder;
 
 use herder::Mastodon;
+use herder::client::Client;
 
 #[test]
 fn mastodon_node_created() {
-    let mastodon_node = Mastodon::new("http://localhost");
-    assert_eq!(mastodon_node.endpoint, String::from("http://localhost"));
+    let Mastodon(mastodon_url) = Mastodon::new("http://localhost");
+    assert_eq!(mastodon_url, String::from("http://localhost"));
+}
+
+#[test]
+fn create_client_with_mastodon_domain() {
+    let mastodon = Mastodon::new("http://localhost");
+    let Client { url_base: mastodon_domain, .. } = mastodon.client(&mastodon.0, "MYTOKEN");
+    assert_eq!(mastodon_domain, String::from("http://localhost"));
+}
+
+#[test]
+fn create_client_with_bearer_token() {
+    let mastodon = Mastodon::new("http://localhost");
+    let Client { bearer_token: token, .. } = mastodon.client(&mastodon.0, "MYTOKEN");
+    assert_eq!(token, String::from("MYTOKEN"));
 }
