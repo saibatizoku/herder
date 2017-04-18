4dc0e8ac46a06fdfa722824ef062fbbf33df2df0
Joaquín R <globojorro@gmail.com>
2017-04-18 02:46:20 -0500

    new tests for basic client functionality

--- tests/test_client.rs
+++ tests/test_client.rs
@@ -5,20 +5,32 @@ use herder::client::Client;
 
 #[test]
 fn mastodon_node_created() {
-    let Mastodon(mastodon_url) = Mastodon::new("http://localhost");
-    assert_eq!(mastodon_url, String::from("http://localhost"));
+    let Mastodon(domain) = Mastodon::new("http://localhost:3000/");
+    assert_eq!(domain, String::from("http://localhost:3000/"));
 }
 
 #[test]
 fn create_client_with_mastodon_domain() {
-    let mastodon = Mastodon::new("http://localhost");
-    let Client { url_base: mastodon_domain, .. } = mastodon.client(&mastodon.0, "MYTOKEN");
-    assert_eq!(mastodon_domain, String::from("http://localhost"));
+    let mastodon = Mastodon::new("http://localhost:3000/");
+    let Client { url_base: domain, .. } = mastodon.client("MYTOKEN");
+    assert_eq!(domain, String::from("http://localhost:3000/"));
 }
 
 #[test]
 fn create_client_with_bearer_token() {
-    let mastodon = Mastodon::new("http://localhost");
-    let Client { bearer_token: token, .. } = mastodon.client(&mastodon.0, "MYTOKEN");
+    let mastodon = Mastodon::new("http://localhost:3000/");
+    let Client { bearer_token: token, .. } = mastodon.client("MYTOKEN");
     assert_eq!(token, String::from("MYTOKEN"));
+}
+
+#[test]
+fn client_builds_api_endpoints() {
+    let mastodon = Mastodon::new("http://localhost:3000/");
+    let api_endpoint = mastodon.endpoint_url("/api/v1/");
+    assert_eq!(api_endpoint, String::from("http://localhost:3000/api/v1/"));
+}
+
+#[test]
+fn client_publish_a_toot() {
+    unimplemented!()
 }
