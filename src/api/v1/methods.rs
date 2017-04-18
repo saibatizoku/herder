0fdf36387e7bfc1671acc417c45ab2bd1e792438
Joaquín R <globojorro@gmail.com>
2017-04-18 03:27:00 -0500

    startup code for API methods

---
+++ src/api/v1/methods.rs
@@ -0,0 +1,58 @@
+pub struct ApiMethod {
+    pub endpoint: String,
+    pub method: AccountsMethod
+}
+
+pub enum AccountsMethod {
+    FetchAccount,
+    GetCurrentUser,
+    UpdateCurrentUser,
+    GetAccountFollowers,
+    GetFollowing,
+    GetAccountStatuses,
+    FollowAccount,
+    UnfollowAccount,
+    MuteAccount,
+    UnmuteAccount,
+    GetAccountRelationships,
+    SearchAccounts
+}
+
+pub enum AppsMethod {
+}
+
+pub enum BlocksMethod {
+}
+
+pub enum FavoritesMethod {
+}
+
+pub enum FollowRequestsMethod {
+}
+
+pub enum FollowsMethod {
+}
+
+pub enum InstancesMethod {
+}
+
+pub enum MediaMethod {
+}
+
+pub enum MutesMethod {
+}
+
+pub enum NotificationsMethod {
+}
+
+pub enum ReportsMethod {
+}
+
+pub enum SearchMethod {
+}
+
+pub enum StatusesMethod {
+}
+
+pub enum TimelinesMethod {
+}
--- src/api/v1/mod.rs
+++ src/api/v1/mod.rs
@@ -1,4 +1,5 @@
 pub mod entities;
+pub mod methods;
 
 pub use self::entities::{
     Account,
