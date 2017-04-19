749f1815462defee21043ef46f81919f7ddf2903
Joaquín R <globojorro@gmail.com>
2017-04-18 20:42:18 -0500

    skeleton tests for ALL API METHODS

--- tests/test_api_methods.rs
+++ tests/test_api_methods.rs
@@ -60,15 +60,128 @@ fn client_search_accounts() {
 }
 
 // API Methods: Apps
+#[test]
+fn client_registering_an_application() {
+    unimplemented!()
+}
+
 // API Methods: Blocks
-// API Methods: Favorites
+#[test]
+fn client_fetching_users_blocks() {
+    unimplemented!()
+}
+
+// API Methods: Favourites
+fn client_fetching_users_favourites() {
+    unimplemented!()
+}
+
 // API Methods: FollowRequests
+fn client_fetching_follow_requests() {
+    unimplemented!()
+}
+
+fn client_authorizing_follow_requests() {
+    unimplemented!()
+}
+
+fn client_rejecting_follow_requests() {
+    unimplemented!()
+}
+
 // API Methods: Follows
+fn client_following_a_remote_user() {
+    unimplemented!()
+}
+
 // API Methods: Instances
+fn client_getting_instance_information() {
+    unimplemented!()
+}
+
 // API Methods: Media
+fn client_uploading_a_media_attachment() {
+    unimplemented!()
+}
+
 // API Methods: Mutes
+fn client_fetchin_a_users_mutes() {
+    unimplemented!()
+}
+
 // API Methods: Notifications
+fn client_fetching_users_notifications() {
+    unimplemented!()
+}
+
+fn client_getting_single_notification() {
+    unimplemented!()
+}
+
+fn client_clearing_notifications() {
+    unimplemented!()
+}
+
 // API Methods: Reports
+fn client_fetching_users_reports() {
+    unimplemented!()
+}
+
+fn client_reporting_user() {
+    unimplemented!()
+}
+
 // API Methods: Search
+fn client_searching_for_content() {
+    unimplemented!()
+}
+
 // API Methods: Statuses
+fn client_fetching_a_status() {
+    unimplemented!()
+}
+
+fn client_getting_status_context() {
+    unimplemented!()
+}
+
+fn client_getting_card_associated_with_status() {
+    unimplemented!()
+}
+
+fn client_getting_who_reblogged_status() {
+    unimplemented!()
+}
+
+fn client_getting_who_favourited_status() {
+    unimplemented!()
+}
+
+fn client_posting_new_status() {
+    unimplemented!()
+}
+
+fn client_deleting_a_status() {
+    unimplemented!()
+}
+
+fn client_reblogging_status() {
+    unimplemented!()
+}
+
+fn client_unreblogging_status() {
+    unimplemented!()
+}
+
+fn client_favouriting_status() {
+    unimplemented!()
+}
+
+fn client_unfavouriting_status() {
+    unimplemented!()
+}
+
 // API Methods: Timelines
+fn client_retrieving_a_timeline() {
+    unimplemented!()
+}
