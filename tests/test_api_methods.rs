53077e7cc530f7d0c39dc99aedc088024db1ec4e
Joaquín R <globojorro@gmail.com>
2017-04-18 20:23:54 -0500

    test skeletons for API Accounts Methods

--- tests/test_api_methods.rs
+++ tests/test_api_methods.rs
@@ -1,5 +1,6 @@
+// API Methods: Accounts
 #[test]
-fn client_fetches_accounts() {
+fn client_fetches_account() {
     unimplemented!()
 }
 
@@ -7,3 +8,59 @@ fn client_fetches_accounts() {
 fn client_gets_current_user() {
     unimplemented!()
 }
+
+#[test]
+fn client_update_current_user() {
+    unimplemented!()
+}
+
+#[test]
+fn client_get_account_followers() {
+    unimplemented!()
+}
+
+#[test]
+fn client_get_who_account_is_following() {
+    unimplemented!()
+}
+
+#[test]
+fn client_get_account_statuses() {
+    unimplemented!()
+}
+
+#[test]
+fn client_follow_account() {
+    unimplemented!()
+}
+
+#[test]
+fn client_unfullow_account() {
+    unimplemented!()
+}
+
+#[test]
+fn client_mute_account() {
+    unimplemented!()
+}
+
+#[test]
+fn client_unmute_account() {
+    unimplemented!()
+}
+
+#[test]
+fn client_get_account_relationships() {
+    unimplemented!()
+}
+
+#[test]
+fn client_search_accounts() {
+    unimplemented!()
+}
+
+// API Methods: Accounts
+// #[test]
+// fn client_() {
+//     unimplemented!()
+// }
