1fe1c057664066539e3235a3177b0d04daf3d3d5
Joaquín R <globojorro@gmail.com>
2017-05-12 02:53:57 -0500

    remaining skeleton code for ALL remaining API Methods!

--- src/api/v1/client.rs
+++ src/api/v1/client.rs
@@ -97,28 +97,100 @@ impl methods::FollowRequests for Client {
 }
 
 impl methods::Follows for Client {
+    fn follow_remote_user(&self, form_data: String) -> Result<entities::Account, &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Instances for Client {
+    fn get_instance(&self) -> Result<entities::Instance, &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Media for Client {
+    fn upload_media(&self, form_data: String) -> Result<entities::Attachment, &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Mutes for Client {
+    fn fetch_mutes(&self, query: String) -> Result<Vec<entities::Account>, &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Notifications for Client {
+    fn fetch_notifications(&self, query: String) -> Result<Vec<entities::Notification>, &str> {
+        unimplemented!()
+    }
+    fn get_notification(&self, notification_id: usize) -> Result<entities::Notification, &str> {
+        unimplemented!()
+    }
+    fn clear_notifications(&self) -> Result<(), &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Reports for Client {
+    fn fetching_reports(&self) -> Result<Vec<entities::Report>, &str> {
+        unimplemented!()
+    }
+    fn reporting_user(&self, form_data: String) -> Result<entities::Report, &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Search for Client {
+    fn search_content(&self, query: String) -> Result<entities::Results, &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Statuses for Client {
+    fn fetch_status(&self, status_id: usize) -> Result<entities::Status, &str> {
+        unimplemented!()
+    }
+    fn get_status_context(&self, status_id: usize) -> Result<entities::Status, &str> {
+        unimplemented!()
+    }
+    fn get_status_card(&self, status_id: usize) -> Result<entities::Card, &str> {
+        unimplemented!()
+    }
+    fn reblogged_by(&self, status_id: usize) -> Result<Vec<entities::Account>, &str> {
+        unimplemented!()
+    }
+    fn favourited_by(&self, status_id: usize) -> Result<Vec<entities::Account>, &str> {
+        unimplemented!()
+    }
+    fn post_status(&self, form_data: String) -> Result<entities::Status, &str> {
+        unimplemented!()
+    }
+    fn delete_status(&self, status_id: usize) -> Result<(), &str> {
+        unimplemented!()
+    }
+    fn reblog_status(&self, status_id: usize) -> Result<entities::Status, &str> {
+        unimplemented!()
+    }
+    fn unreblog_status(&self, status_id: usize) -> Result<entities::Status, &str> {
+        unimplemented!()
+    }
+    fn favourite_status(&self, status_id: usize) -> Result<entities::Status, &str> {
+        unimplemented!()
+    }
+    fn unfavourite_status(&self, status_id: usize) -> Result<entities::Status, &str> {
+        unimplemented!()
+    }
 }
 
 impl methods::Timelines for Client {
+    fn home_timeline(&self, query: String) -> Result<Vec<entities::Status>, &str> {
+        unimplemented!()
+    }
+    fn public_timeline(&self, query: String) -> Result<Vec<entities::Status>, &str> {
+        unimplemented!()
+    }
+    fn tag_timeline(&self, hashtag: String, query: String) -> Result<Vec<entities::Status>, &str> {
+        unimplemented!()
+    }
 }
--- src/api/v1/methods.rs
+++ src/api/v1/methods.rs
@@ -296,28 +296,353 @@ pub trait FollowRequests {
 }
 
 pub trait Follows {
+    /// Following a remote user:
+    ///
+    /// ```
+    /// POST /api/v1/follows
+    /// ```
+    ///
+    /// Form data:
+    ///
+    /// `uri` `username@domain` of the person you want to follow. It is required.
+    ///
+    /// Returns the local representation of the followed `Account`.
+    fn follow_remote_user(&self, form_data: String) -> Result<entities::Account, &str>;
 }
 
 pub trait Instances {
+    /// Getting instance information:
+    ///
+    /// ```
+    /// GET /api/v1/instance
+    /// ```
+    ///
+    /// Returns the current `Instance`.
+    ///
+    /// Does not require authentication.
+    fn get_instance(&self) -> Result<entities::Instance, &str>;
 }
 
 pub trait Media {
+    /// Uploading a media attachment:
+    ///
+    /// ```
+    /// POST /api/v1/media
+    /// ```
+    ///
+    /// Form data:
+    ///
+    /// `file` Media to be uploaded. It is required.
+    ///
+    /// Returns an `Attachment`that can be used when creating a status.
+    fn upload_media(&self, form_data: String) -> Result<entities::Attachment, &str>;
 }
 
 pub trait Mutes {
+    /// Fetching a user's mutes
+    ///
+    /// ```
+    /// GET /api/v1/mutes
+    /// ```
+    ///
+    /// Query parameters:
+    ///
+    /// `max_id` Get a list of mutes with ID less than or equal this value. It is optional.
+    ///
+    /// `since_id` Get a list of mutes with ID greater than this value. It is optional.
+    ///
+    /// `limit` Maximum number of mutes to get (Default 40, Max 80). It is optional.
+    ///
+    /// Returns an array of `Account`s muted by the authenticated user.
+    fn fetch_mutes(&self, query: String) -> Result<Vec<entities::Account>, &str>;
 }
 
 pub trait Notifications {
+    /// Fetching a user's notifications:
+    ///
+    /// ```
+    /// GET /api/v1/notifications
+    /// ```
+    ///
+    /// Query parameters:
+    ///
+    /// `max_id` Get a list of notifications with ID less than or equal this value. It is optional.
+    ///
+    /// `since_id` Get a list of notifications with ID greater than this value. It is optional.
+    ///
+    /// `limit` Maximum number of notifications to get (Default 40, Max 80). It is optional.
+    ///
+    /// Returns an array of `Notification`s for the authenticated user.
+    fn fetch_notifications(&self, query: String) -> Result<Vec<entities::Notification>, &str>;
+
+    /// Getting a single notification:
+    ///
+    /// ```
+    /// GET /api/v1/notifications/:notification_id
+    /// ```
+    ///
+    /// Returns the `Notification`
+    fn get_notification(&self, notification_id: usize) -> Result<entities::Notification, &str>;
+
+    /// Clearing notifications:
+    ///
+    /// ```
+    /// POST /api/v1/notifications/clear
+    /// ```
+    ///
+    /// Deletes all notifications from the Mastodon server for the authenticated user. Returns an
+    /// empty object.
+    fn clear_notifications(&self) -> Result<(), &str>;
 }
 
 pub trait Reports {
+    /// Fetching a user's reports:
+    ///
+    /// ```
+    /// GET /api/v1/reports
+    /// ```
+    ///
+    /// Returns a list of `Report`s made by the authenticated user..
+    fn fetching_reports(&self) -> Result<Vec<entities::Report>, &str>;
+
+    /// Reporting a user:
+    ///
+    /// ```
+    /// POST /api/v1/reports
+    /// ```
+    ///
+    /// Form data:
+    ///
+    /// `account_id` The ID of the account to report. It is required.
+    ///
+    /// `status_ids` The IDs of the status to report (can be an array). It is required.
+    ///
+    /// `comment` A comment to associate with the report. It is required.
+    ///
+    /// Returns the finished `Report`.
+    fn reporting_user(&self, form_data: String) -> Result<entities::Report, &str>;
 }
 
 pub trait Search {
+    /// Searching for content:
+    ///
+    /// ```
+    /// GET /api/v1/search
+    /// ```
+    ///
+    /// Form data:
+    ///
+    /// `q` The search query. It is required.
+    ///
+    /// `resolve` Whether to resolve non-local accounts. It is required.
+    ///
+    /// Returns `Results`.
+    ///
+    /// If `q` is a URL, Mastodon will attempt to fetch the provided account or status. Otherwise,
+    /// it will do a local account and hashtag search.
+    ///
+    /// Does not require authentication.
+    fn search_content(&self, query: String) -> Result<entities::Results, &str>;
 }
 
 pub trait Statuses {
+    /// Fetching a status:
+    ///
+    /// ```
+    /// GET /api/v1/statuses/:status_id
+    /// ```
+    ///
+    /// Returns a `Status`.
+    ///
+    /// Does not require authentication.
+    fn fetch_status(&self, status_id: usize) -> Result<entities::Status, &str>;
+
+    /// Getting status context:
+    ///
+    /// ```
+    /// GET /api/v1/statuses/:status_id/context
+    /// ```
+    ///
+    /// Returns a `Context`.
+    ///
+    /// Does not require authentication.
+    fn get_status_context(&self, status_id: usize) -> Result<entities::Status, &str>;
+
+    /// Getting a card associated with a status:
+    ///
+    /// ```
+    /// GET /api/v1/statuses/:status_id/card
+    /// ```
+    ///
+    /// Returns a `Card`.
+    ///
+    /// Does not require authentication.
+    fn get_status_card(&self, status_id: usize) -> Result<entities::Card, &str>;
+
+
+    /// Getting who reblogged a status:
+    ///
+    /// ```
+    /// GET /api/v1/statuses/:status_id/reblogged_by
+    /// ```
+    ///
+    /// Query parameters:
+    ///
+    /// `max_id`  Get a list of reblogged with ID less than or equal this value    yes
+    ///
+    /// `since_id`    Get a list of reblogged with ID greater than this value  yes
+    ///
+    /// `limit`   Maximum number of reblogged to get (Default 40, Max 80)  yes
+    ///
+    /// Returns an array of `Account`s.
+    ///
+    /// Does not require authentication.
+    fn reblogged_by(&self, status_id: usize) -> Result<Vec<entities::Account>, &str>;
+
+    /// Getting who favourited a status:
+    ///
+    /// ```
+    /// GET /api/v1/statuses/:status_id/favourited_by
+    /// ```
+    ///
+    /// Query parameters:
+    ///
+    /// `max_id`  Get a list of favourited with ID less than or equal this value    yes
+    ///
+    /// `since_id`    Get a list of favourited with ID greater than this value  yes
+    ///
+    /// `limit`   Maximum number of favourited to get (Default 40, Max 80)  yes
+    ///
+    /// Returns an array of `Account`s.
+    ///
+    /// Does not require authentication.
+    fn favourited_by(&self, status_id: usize) -> Result<Vec<entities::Account>, &str>;
+
+    /// Posting a new status:
+    ///
+    /// ```
+    /// POST /api/v1/statuses
+    /// ```
+    ///
+    /// Form data:
+    ///
+    /// `status`  The text of the status  no
+    /// `in_reply_to_id`  local ID of the status you want to reply to. It is optional.
+    /// `media_ids`   Array of media IDs to attach to the status (maximum 4). It is optional.    /// `sensitive`   Set this to mark the media of the status as NSFW. It is optional.
+    /// `spoiler_text`    Text to be shown as a warning before the actual content. It is optional.
+    /// `visibility`  Either "direct", "private", "unlisted" or "public". It is optional.
+    ///
+    /// Returns the new `Status`.
+    fn post_status(&self, form_data: String) -> Result<entities::Status, &str>;
+
+    /// Deleting a status:
+    ///
+    /// ```
+    /// DELETE /api/v1/statuses/:status_id
+    /// ```
+    ///
+    /// Returns an empty object.
+    fn delete_status(&self, status_id: usize) -> Result<(), &str>;
+
+    /// Reblogging a status:
+    ///
+    /// ```
+    /// POST /api/v1/statuses/:status_id/reblog
+    /// ```
+    ///
+    /// Returns the target Status.
+    fn reblog_status(&self, status_id: usize) -> Result<entities::Status, &str>;
+
+    /// Unreblogging a status:
+    ///
+    /// ```
+    /// POST /api/v1/statuses/:status_id/unreblog
+    /// ```
+    ///
+    /// Returns the target Status.
+    fn unreblog_status(&self, status_id: usize) -> Result<entities::Status, &str>;
+
+    /// Favouriting/unfavouriting a status:
+    ///
+    /// ```
+    /// POST /api/v1/statuses/:status_id/favourite
+    /// ```
+    ///
+    /// Returns the target Status.
+    fn favourite_status(&self, status_id: usize) -> Result<entities::Status, &str>;
+
+    /// Favouriting/unfavouriting a status:
+    ///
+    /// ```
+    /// POST /api/v1/statuses/:status_id/unfavourite
+    /// ```
+    ///
+    /// Returns the target Status.
+    fn unfavourite_status(&self, status_id: usize) -> Result<entities::Status, &str>;
 }
 
 pub trait Timelines {
+    /// Retrieving a home timeline:
+    ///
+    /// ```
+    /// GET /api/v1/timelines/home
+    /// ```
+    ///
+    /// Query parameters:
+    ///
+    /// `local`   Only return statuses originating from this instance (public and tag timelines only). It is optional.
+    ///
+    /// `max_id`  Get a list of timelines with ID less than or equal this value. It is optional.
+    ///
+    /// `since_id`    Get a list of timelines with ID greater than this value. It is optional.
+    ///
+    /// `limit`   Maximum number of statuses on the requested timeline to get (Default 20, Max 40).
+    /// It is optional.
+    ///
+    /// Returns an array of `Status`es, most recent ones first.
+    fn home_timeline(&self, query: String) -> Result<Vec<entities::Status>, &str>;
+
+    /// Retrieving a public timeline:
+    ///
+    /// ```
+    /// GET /api/v1/timelines/public
+    /// ```
+    ///
+    /// Query parameters:
+    ///
+    /// `local`   Only return statuses originating from this instance (public and tag timelines only). It is optional.
+    ///
+    /// `max_id`  Get a list of timelines with ID less than or equal this value. It is optional.
+    ///
+    /// `since_id`    Get a list of timelines with ID greater than this value. It is optional.
+    ///
+    /// `limit`   Maximum number of statuses on the requested timeline to get (Default 20, Max 40).
+    /// It is optional.
+    ///
+    /// Returns an array of `Status`es, most recent ones first.
+    ///
+    /// Does not require authentication.
+    fn public_timeline(&self, query: String) -> Result<Vec<entities::Status>, &str>;
+
+    /// Retrieving a timeline:
+    ///
+    /// ```
+    /// GET /api/v1/timelines/tag/:hashtag
+    /// ```
+    ///
+    /// Query parameters:
+    ///
+    /// `local`   Only return statuses originating from this instance (public and tag timelines only). It is optional.
+    ///
+    /// `max_id`  Get a list of timelines with ID less than or equal this value. It is optional.
+    ///
+    /// `since_id`    Get a list of timelines with ID greater than this value. It is optional.
+    ///
+    /// `limit`   Maximum number of statuses on the requested timeline to get (Default 20, Max 40).
+    /// It is optional.
+    ///
+    /// Returns an array of `Status`es, most recent ones first.
+    ///
+    /// Does not require authentication.
+    fn tag_timeline(&self, hashtag: String, query: String) -> Result<Vec<entities::Status>, &str>;
 }
